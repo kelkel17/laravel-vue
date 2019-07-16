032935b7ebcebc527f6d42005a8acb6ccea3d535
christanplaza <cosmotechnologist@gmail.com>
2019-07-16 15:27:04 +0800

    Minor UI fixes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -36,7 +36,7 @@
 
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <a href="index3.html" class="brand-link">
+            <a href="/dashboard" class="brand-link">
                 <img src="/img/settings.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                 style="opacity: .8">
                 <span class="brand-text font-weight-light">Laravel Vue</span>
@@ -49,7 +49,7 @@
                         <img src="/img/customer-service.png" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+                        <a href="/profile" class="d-block">{{ Auth::user()->name }}</a>
                     </div>
                 </div>
 
@@ -94,7 +94,9 @@
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
                                 <i class="nav-icon fas fa-power-off text-danger"></i>
-                                {{ __('Logout') }}
+                                <p>
+                                    {{ __('Logout') }}
+                                </p>
                             </a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
